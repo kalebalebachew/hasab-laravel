3edff1c7cc9e576c2f75f609d779e96402ff187d
kalebalebachew <kalebalebachew4@gmail.com>
2025-11-12 19:09:17 +0300

    Update Hasab API base URL in configuration file to include '/api'

--- config/hasab.php
+++ config/hasab.php
@@ -2,6 +2,6 @@
 
 return [
     'key' => env('HASAB_API_KEY', ''),
-    'base_url' => env('HASAB_BASE_URL', 'https://hasab.co'),
+    'base_url' => env('HASAB_BASE_URL', 'https://hasab.co/api'),
     'version' => env('HASAB_API_VERSION', 'v1'),
 ];
