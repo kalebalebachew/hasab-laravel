9b73e5c9a9ee613cb92f4df45f84a351ab0141b9
kalebalebachew <kalebalebachew4@gmail.com>
2025-11-13 12:07:42 +0300

    refactor: refactor transcription service to use the correct pattern from the docs

--- src/Services/TranscriptionService.php
+++ src/Services/TranscriptionService.php
@@ -10,35 +10,11 @@ class TranscriptionService
 
     public function upload(array $options): array
     {
-        // If local file is provided, upload it first
-        if (isset($options['file']) && !isset($options['url'])) {
-            return $this->uploadFile($options);
-        }
+        $file = $options['file'];
 
-        // Process audio from URL (S3 or external)
-        return $this->http->postJson('upload-audio', [
-            'url' => $options['url'],
-            'key' => $options['key'],
-            'uuid' => $options['uuid'] ?? null,
-            'is_meeting' => $options['is_meeting'] ?? false,
-            'transcribe' => $options['transcribe'] ?? true,
-            'translate' => $options['translate'] ?? false,
-            'summarize' => $options['summarize'] ?? false,
-            'language' => $options['language'] ?? 'auto',
-            'source_language' => $options['source_language'] ?? null,
-            'timestamps' => $options['timestamps'] ?? false,
-        ]);
-    }
-
-    protected function uploadFile(array $options): array
-    {
-        // Upload file to get S3 URL first (if you have a file upload endpoint)
-        // For now, we'll use multipart directly
         return $this->http->postMultipart('upload-audio', [
-            'file' => $options['file'],
+            'file' => $file,
         ], [
-            'uuid' => $options['uuid'] ?? null,
-            'is_meeting' => $options['is_meeting'] ?? false,
             'transcribe' => $options['transcribe'] ?? true,
             'translate' => $options['translate'] ?? false,
             'summarize' => $options['summarize'] ?? false,
